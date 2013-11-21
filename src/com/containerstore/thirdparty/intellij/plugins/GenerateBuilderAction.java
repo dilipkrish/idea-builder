7bf8da5dc71afc2bc625c58518abb5565c0fc8c0
Dilip Krishnan <d_krishnan@containerstore.com>
2013-11-21 10:56:04 -0600

    Adding a helper method to get psiClass

--- src/com/containerstore/thirdparty/intellij/plugins/GenerateBuilderAction.java
+++ src/com/containerstore/thirdparty/intellij/plugins/GenerateBuilderAction.java
@@ -2,9 +2,30 @@ package com.containerstore.thirdparty.intellij.plugins;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.LangDataKeys;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.util.PsiTreeUtil;
 
 public class GenerateBuilderAction extends AnAction {
     public void actionPerformed(AnActionEvent e) {
         // TODO: insert action logic here
+    }
+
+    private PsiClass psiClassFromContext(AnActionEvent e) {
+        PsiFile psiFile = e.getData(LangDataKeys.PSI_FILE);
+        Editor editor = e.getData(LangDataKeys.EDITOR);
+        if (psiFile == null || editor == null) {
+            return null;
+        }
+        int offset = editor.getCaretModel().getOffset();
+        PsiElement elementAt = psiFile.findElementAt(offset);
+        PsiClass parentClass = PsiTreeUtil.getParentOfType(elementAt, PsiClass.class);
+        if (parentClass == null) {
+            return parentClass;
+        }
+        return parentClass;
     }
 }
