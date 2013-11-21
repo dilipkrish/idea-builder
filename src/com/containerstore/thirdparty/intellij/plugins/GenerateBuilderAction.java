07b7444eb359d58370a9154e324a5afb4b39d62c
Dilip Krishnan <d_krishnan@containerstore.com>
2013-11-21 11:20:35 -0600

    Show the dialog and if stub generation of builder
    (cherry picked from commit 5ae50e23daaea80ff55dd4ea783b4edf672e637a)

--- src/com/containerstore/thirdparty/intellij/plugins/GenerateBuilderAction.java
+++ src/com/containerstore/thirdparty/intellij/plugins/GenerateBuilderAction.java
@@ -6,12 +6,22 @@ import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiField;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.ui.CollectionListModel;
 
 public class GenerateBuilderAction extends AnAction {
     public void actionPerformed(AnActionEvent e) {
-        maybeEnablePresentation(e);
+        PsiClass psiClass = maybeEnablePresentation(e);
+        FieldSelectionDialog dialog = new FieldSelectionDialog(psiClass);
+        dialog.show();
+        if (dialog.isOK()) {
+            CollectionListModel<PsiField> fields = dialog.getFields();
+            //TODO:
+            // 1. Generate the builder based on the selected fields
+            // 2. Add it to the file that is currently being edited
+        }
     }
 
     @Override
@@ -19,9 +29,10 @@ public class GenerateBuilderAction extends AnAction {
         maybeEnablePresentation(e);
     }
 
-    private void maybeEnablePresentation(AnActionEvent e) {
+    private PsiClass maybeEnablePresentation(AnActionEvent e) {
         PsiClass psiClass = psiClassFromContext(e);
         e.getPresentation().setEnabled(psiClass != null);
+        return psiClass;
     }
 
     private PsiClass psiClassFromContext(AnActionEvent e) {
