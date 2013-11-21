d6ba2aa3c20958841efa6fdafe8476242305f73c
Dilip Krishnan <d_krishnan@containerstore.com>
2013-11-20 20:48:03 -0600

    After plugin action added via wizard

---
+++ src/com/containerstore/thirdparty/intellij/plugins/GenerateBuilderAction.java
@@ -0,0 +1,10 @@
+package com.containerstore.thirdparty.intellij.plugins;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+public class GenerateBuilderAction extends AnAction {
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
